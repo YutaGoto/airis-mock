bb3231377980ac5f343a7be48e792540fb58cce5
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2025-02-12 00:45:27 +0900

    :sparkles: add get_body_type

--- src/models/body_type.rs
+++ src/models/body_type.rs
@@ -18,3 +18,24 @@ pub fn get_random_body_type() -> BodyType {
         cd: shape_codes[random_index].code,
     }
 }
+
+pub fn get_body_type(chassisno: String) -> BodyType {
+    let v = chassisno.split("-").collect::<Vec<&str>>();
+    let num = v[0].to_string();
+    let shape_codes = read_shape_codes();
+
+    let result = shape_codes.iter().find(|shape_code| {
+        let code = shape_code.code.to_string();
+        let num_str = num.chars().take(2).collect::<String>();
+        code.starts_with(&num_str)
+    });
+
+    if let Some(shape_code) = result {
+        BodyType {
+            name: shape_code.body_type.clone(),
+            cd: shape_code.code,
+        }
+    } else {
+        get_random_body_type()
+    }
+}
